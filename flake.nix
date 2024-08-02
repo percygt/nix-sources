ed49b7cff0a527ae23b34c78b7ba0901bd554ae6
Percy Timon <percygt.dev@gmail.com>
2024-08-02 17:52:35 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   description = "PercyGT's nix sources";
   nixConfig = {
-    extra-substituters = ["https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   inputs = {
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
@@ -28,44 +30,52 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = {
-    nixpkgs,
-    self,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = ["aarch64-linux" "i686-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin"];
-    forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
-    overlays = {
-      emacs = inputs.emacs-overlay.overlay;
-      swayfx-unwrapped = inputs.swayfx-unwrapped.overlays.default;
-      neovim-nightly = inputs.neovim-nightly-overlay.overlays.default;
-    };
-    legacyPackages = forEachSystem (
-      system:
+  outputs =
+    { self, ... }@inputs:
+    let
+      inherit (self) outputs;
+      systems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forEachSystem = inputs.nixpkgs.lib.genAttrs systems;
+      overlays = {
+        emacs = inputs.emacs-overlay.overlay;
+        swayfx-unwrapped = inputs.swayfx-unwrapped.overlays.default;
+        neovim-nightly = inputs.neovim-nightly-overlay.overlays.default;
+      };
+      legacyPackages = forEachSystem (
+        system:
         import inputs.nixpkgs {
           inherit system;
           overlays = builtins.attrValues overlays;
           config.allowUnfree = true;
         }
-    );
-  in {
-    packages = forEachSystem (system: let
-      pkgs = legacyPackages.${system};
-    in {
-      swayfx-src = pkgs.callPackage ({swayfx}: swayfx.override {inherit (pkgs) swayfx-unwrapped;}) {};
-      emacs-unstable-pgtk = pkgs.callPackage ({emacs-unstable-pgtk}: emacs-unstable-pgtk.override {withTreeSitter = true;}) {};
-      neovim-unstable = pkgs.callPackage ({neovim}: neovim) {};
-    });
-    overlays = {
-      default = final: prev: {
-        inherit
-          (outputs.packages.${prev.system})
-          swayfx-src
-          emacs-unstable-pgtk
-          neovim-unstable
-          ;
+      );
+    in
+    {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = legacyPackages.${system};
+        in
+        {
+          swayfx-src = pkgs.callPackage (
+            { swayfx }: swayfx.override { inherit (pkgs) swayfx-unwrapped; }
+          ) { };
+          emacs-unstable-pgtk = pkgs.callPackage (
+            { emacs-unstable-pgtk }: emacs-unstable-pgtk.override { withTreeSitter = true; }
+          ) { };
+          neovim-unstable = pkgs.callPackage ({ neovim }: neovim) { };
+        }
+      );
+      overlays = {
+        default = final: prev: {
+          inherit (outputs.packages.${prev.system}) swayfx-src emacs-unstable-pgtk neovim-unstable;
+        };
       };
     };
-  };
 }
