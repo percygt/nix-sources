ab2e7840e4a0071f4931ce84ae9d592b2c6ff5ff
Percy Timon <percygt.dev@gmail.com>
2025-11-17 04:13:11 +0800

    update

--- flake.nix
+++ flake.nix
@@ -5,13 +5,11 @@
       "https://percygtdev.cachix.org"
       "https://nix-community.cachix.org"
       "https://niri.cachix.org"
-      "https://watersucks.cachix.org"
     ];
     extra-trusted-public-keys = [
       "percygtdev.cachix.org-1:AGd4bI4nW7DkJgniWF4tS64EX2uSYIGqjZih2UVoxko="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-      "watersucks.cachix.org-1:6gadPC5R8iLWQ3EUtfu3GFrVY7X6I4Fwz/ihW25Jbv8="
     ];
   };
   inputs = {
@@ -21,11 +19,8 @@
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs.follows = "nixpkgs-unstable";
 
-    nixos-cli.url = "github:nix-community/nixos-cli";
     niri.url = "github:sodiboo/niri-flake";
-    # swayfx-unwrapped-git.url = "github:WillPower3309/swayfx";
-    emacs-overlay.url = "github:nix-community/emacs-overlay/";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    swayfx-unwrapped-git.url = "github:WillPower3309/swayfx";
   };
   outputs =
     { self, ... }@inputs:
@@ -38,8 +33,6 @@
       inherit (inputs.nixpkgs) lib;
       forEachSystem = lib.genAttrs systems;
       overlays = {
-        emacs = inputs.emacs-overlay.overlays.default;
-        neovim-nightly = inputs.neovim-nightly-overlay.overlays.default;
         niri-flake = inputs.niri.overlays.niri;
       };
       packagesFrom =
@@ -61,87 +54,61 @@
     in
     {
       formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
-      packages = forAllSystems (
-        pkgs:
-        let
-          inherit (pkgs.stdenv.hostPlatform) system;
-        in
-        {
-          emacs-unstable = pkgs.callPackage (
-            { emacs-unstable }:
-            emacs-unstable.override {
-              withTreeSitter = true;
-            }
-          ) { };
-          niri-stable = pkgs.callPackage ({ niri-stable }: niri-stable) { };
-          niri-unstable = pkgs.callPackage ({ niri-unstable }: niri-unstable) { };
-          xwayland-satellite-stable = pkgs.callPackage (
-            { xwayland-satellite-stable }: xwayland-satellite-stable
-          ) { };
-          xwayland-satellite-unstable = pkgs.callPackage (
-            { xwayland-satellite-unstable }: xwayland-satellite-unstable
-          ) { };
-          neovim-unstable = pkgs.callPackage ({ neovim }: neovim) { };
-          nixos = inputs.nixos-cli.packages.${system}.default;
-
-          mesa = pkgs.callPackage ({ mesa }: mesa) { };
-          mesa-32 = pkgs.callPackage ({ pkgsi686Linux }: pkgsi686Linux.mesa) { };
-          intel-vaapi-driver = pkgs.callPackage (
-            { intel-vaapi-driver }:
-            intel-vaapi-driver.override {
-              enableHybridCodec = true;
-            }
-          ) { };
-          intel-vaapi-driver-32 = pkgs.callPackage (
-            { driversi686Linux }:
-            driversi686Linux.intel-vaapi-driver.override {
-              enableHybridCodec = true;
-            }
-          ) { };
-          intel-media-driver = pkgs.callPackage ({ intel-media-driver }: intel-media-driver) { };
-          intel-media-driver-32 = pkgs.callPackage (
-            { driversi686Linux }: driversi686Linux.intel-media-driver
-          ) { };
-          intel-ocl = pkgs.callPackage ({ intel-ocl }: intel-ocl) { };
-          intel-compute-runtime = pkgs.callPackage ({ intel-compute-runtime }: intel-compute-runtime) { };
-          vpl-gpu-rt = pkgs.callPackage ({ vpl-gpu-rt }: vpl-gpu-rt) { };
-
-          # emacs-pgtk = pkgs.callPackage (
-          #   { emacs-pgtk }:
-          #   emacs-pgtk.override {
-          #     withTreeSitter = true;
-          #   }
-          # ) { };
-          # emacs-unstable-pgtk = pkgs.callPackage (
-          #   { emacs-unstable-pgtk }:
-          #   emacs-unstable-pgtk.override {
-          #     withTreeSitter = true;
-          #   }
-          # ) { };
-          # swayfx-git = pkgs.callPackage (
-          #   { swayfx }:
-          #   swayfx.override {
-          #     swayfx-unwrapped =
-          #       inputs.swayfx-unwrapped-git.packages.${pkgs.stdenv.hostPlatform.system}.swayfx-unwrapped-git;
-          #   }
-          # ) { };
-        }
-      );
+      packages = forAllSystems (pkgs: {
+        niri-stable = pkgs.callPackage ({ niri-stable }: niri-stable) { };
+        niri-unstable = pkgs.callPackage ({ niri-unstable }: niri-unstable) { };
+        xwayland-satellite-stable = pkgs.callPackage (
+          { xwayland-satellite-stable }: xwayland-satellite-stable
+        ) { };
+        xwayland-satellite-unstable = pkgs.callPackage (
+          { xwayland-satellite-unstable }: xwayland-satellite-unstable
+        ) { };
+        mesa = pkgs.callPackage ({ mesa }: mesa) { };
+        mesa-32 = pkgs.callPackage ({ pkgsi686Linux }: pkgsi686Linux.mesa) { };
+        intel-vaapi-driver = pkgs.callPackage (
+          { intel-vaapi-driver }:
+          intel-vaapi-driver.override {
+            enableHybridCodec = true;
+          }
+        ) { };
+        intel-vaapi-driver-32 = pkgs.callPackage (
+          { driversi686Linux }:
+          driversi686Linux.intel-vaapi-driver.override {
+            enableHybridCodec = true;
+          }
+        ) { };
+        intel-media-driver = pkgs.callPackage ({ intel-media-driver }: intel-media-driver) { };
+        intel-media-driver-32 = pkgs.callPackage (
+          { driversi686Linux }: driversi686Linux.intel-media-driver
+        ) { };
+        intel-ocl = pkgs.callPackage ({ intel-ocl }: intel-ocl) { };
+        intel-compute-runtime = pkgs.callPackage ({ intel-compute-runtime }: intel-compute-runtime) { };
+        vpl-gpu-rt = pkgs.callPackage ({ vpl-gpu-rt }: vpl-gpu-rt) { };
+        pipewire = pkgs.callPackage ({ pipewire }: pipewire) { };
+        foot = pkgs.callPackage ({ foot }: foot) { };
+        ghostty = pkgs.callPackage ({ ghostty }: ghostty) { };
+        tilix = pkgs.callPackage ({ tilix }: tilix) { };
+        xfce4-terminal = pkgs.callPackage ({ xfce }: xfce.xfce4-terminal) { };
+        wezterm = pkgs.callPackage ({ wezterm }: wezterm) { };
+        brave = pkgs.callPackage ({ brave }: brave) { };
+        swayfx-unstable = pkgs.callPackage (
+          { swayfx }:
+          swayfx.override {
+            swayfx-unwrapped =
+              inputs.swayfx-unwrapped-git.packages.${pkgs.stdenv.hostPlatform.system}.swayfx-unwrapped-git;
+          }
+        ) { };
+      });
 
       overlays = {
         default = final: prev: {
-          inherit (outputs.packages.${prev.stdenv.hostPlatform.system})
-            # swayfx-git
-            nixos
-            emacs-unstable
-            neovim-unstable
-            niri-stable
-            niri-unstable
-            xwayland-satellite-stable
-            xwayland-satellite-unstable
-            ;
-          my = {
+          myBin = {
             inherit (outputs.packages.${prev.stdenv.hostPlatform.system})
+              niri-stable
+              niri-unstable
+              swayfx-unstable
+              xwayland-satellite-stable
+              xwayland-satellite-unstable
               mesa
               mesa-32
               intel-vaapi-driver
@@ -151,6 +118,9 @@
               intel-ocl
               intel-compute-runtime
               vpl-gpu-rt
+              pipewire
+              foot
+              brave
               ;
           };
         };
