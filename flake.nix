6db317c2c983401bd814d428314df0650b540531
Percy Timon <percygt.dev@gmail.com>
2026-05-11 11:41:27 +0800

    Updated flake inputs

--- flake.nix
+++ flake.nix
@@ -13,9 +13,11 @@
     ];
   };
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs-old.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    # channel urls are faster and more reliable than github -.-
+    nixpkgs-stable.url = "https://channels.nixos.org/nixos-25.11/nixexprs.tar.xz";
+    nixpkgs-old.url = "https://channels.nixos.org/nixos-24.11/nixexprs.tar.xz";
+    nixpkgs-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs.follows = "nixpkgs-unstable";
 
